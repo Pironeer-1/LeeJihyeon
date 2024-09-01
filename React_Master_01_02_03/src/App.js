9438e216dd5ea7fa029ded523a5d257ee7887bd6
Jihyeon Lee <ljh130334@naver.com>
2024-09-01 19:57:51 +0900

    [Update]React_Master_01_02_03

---
+++ React_Master_01_02_03/src/App.js
@@ -0,0 +1,24 @@
+import { product } from "prelude-ls";
+
+const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 13 },
+];
+
+export default function ShoppingList() {
+    const listItems = products.map(product =>
+        <li
+            key={product.id}
+            style={{
+                color: product.isFruit ? 'magenta' : 'darkgreen'
+            }}
+        >
+            {product.title}
+        </li>
+    );
+
+    return (
+        <ul>{listItems}</ul>
+    );
+}
