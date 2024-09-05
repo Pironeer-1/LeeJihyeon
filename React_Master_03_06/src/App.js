8114dd7c11d983cbee36e74294bf11280c3a328d
Jihyeon Lee <ljh130334@naver.com>
2024-09-06 00:16:45 +0900

    [Update]React_Master_03_06_04

--- React_Master_03_06/src/App.js
+++ React_Master_03_06/src/App.js
@@ -10,11 +10,25 @@
 //     }
 // }
 
+// function Item({ name, isPacked }) {
+//     if (isPacked) {
+//         return null;
+//     }
+//     return <li className="item">{name}</li>;
+// }
+
 function Item({ name, isPacked }) {
-    if (isPacked) {
-        return null;
-    }
-    return <li className="item">{name}</li>;
+    return (
+        <li className="item">
+            {isPacked ? (
+                <del>
+                    {name + '✔'}
+                </del>
+            ) : (
+                name
+            )}
+        </li>
+    );
 }
 
 export default function PackingList() {
