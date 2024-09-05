0051b0fc0d591008254b61c0497503f4c9fd2496
Jihyeon Lee <ljh130334@naver.com>
2024-09-05 23:20:28 +0900

    [Update]React_Master_03_04_04

--- React_Master_03_04/src/App.js
+++ React_Master_03_04/src/App.js
@@ -20,9 +20,23 @@
 //     );
 // }
 
+// export default function TodoList() {
+//     const name = 'Hedy Lamarr';
+//     return (
+//         <h1>{name}'s To Do List</h1>
+//     );
+// }
+
+const today = new Date();
+function formatDate(date) {
+    return new Intl.DateTimeFormat(
+        'en-US',
+        { weekday: 'long' }
+        ).format(date);
+}
+
 export default function TodoList() {
-    const name = 'Gregorio Y. Zara';
     return (
-        <h1>{name}'s To Do List</h1>
+        <h1>To do List for {formatDate(today)}</h1>
     );
 }
