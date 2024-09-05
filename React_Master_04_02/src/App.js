f421bfd18027f7b9a6e7cc3ea1539440b6268fb0
Jihyeon Lee <ljh130334@naver.com>
2024-09-06 02:01:58 +0900

    [Update]React_Master_04_02_03

--- React_Master_04_02/src/App.js
+++ React_Master_04_02/src/App.js
@@ -4,10 +4,15 @@ import {sculptureList} from './data.js';
 export default function Gallery() {
     //let index = 0;
     const [index, setIndex] = useState(0);
+    const [showMore, setShowMore] = useState(false);
 
     function handleClick() {
         //index = index + 1;
         setIndex(index + 1);
+    }
+
+    function handleMoreClick() {
+        setShowMore(!showMore);
     }
 
     let sculpture = sculptureList[index];
@@ -23,6 +28,10 @@ export default function Gallery() {
             <h3>
                 ({index + 1} of {sculptureList.length})
             </h3>
+            <button onClick={handleMoreClick}>
+                {showMore ? 'Hide' : 'Show'} details
+            </button>
+            {showMore && <p>{sculpture.description}</p>}
             <img
                 src={sculpture.url}
                 alt={sculpture.alt}
