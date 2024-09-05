602eb1e3077be71f5d83cd3a945688c7cfe0b2eb
Jihyeon Lee <ljh130334@naver.com>
2024-09-06 01:42:36 +0900

    [Update]React_Master_04_01_03

--- React_Master_04_01/src/App.js
+++ React_Master_04_01/src/App.js
@@ -1,11 +1,32 @@
-export default function Button() {
-    function handleClick() {
-        alert('You clicked me!');
-    }
+// export default function Button() {
+//     function handleClick() {
+//         alert('You clicked me!');
+//     }
+//
+//     return (
+//         <button onClick={handleClick}>
+//             Click me
+//         </button>
+//     );
+// }
 
+function AlertButton({message, children}) {
     return (
-        <button onClick={handleClick}>
-            Click me
+        <button onClick={() => alert(message)}>
+            {children}
         </button>
+    );
+}
+
+export default function Toolbar() {
+    return (
+        <div>
+            <AlertButton message="Playing!">
+                Play Movie
+            </AlertButton>
+            <AlertButton message="Uploading!">
+                Upload Image
+            </AlertButton>
+        </div>
     );
 }
