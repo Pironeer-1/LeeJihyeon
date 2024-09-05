b8b05c94b9babec7e1cbf858f290b1859fb3fabb
Jihyeon Lee <ljh130334@naver.com>
2024-09-06 01:46:56 +0900

    [Update]React_Master_04_01_06

--- React_Master_04_01/src/App.js
+++ React_Master_04_01/src/App.js
@@ -68,23 +68,53 @@
 //     );
 // }
 
-function Button({onSmash, children}) {
-    return (
-        <button onClick={onSmash}>
-            {children}
-        </button>
-    );
-}
+// function Button({onSmash, children}) {
+//     return (
+//         <button onClick={onSmash}>
+//             {children}
+//         </button>
+//     );
+// }
+//
+// export default function App() {
+//     return (
+//         <div>
+//             <Button onSmash={() => alert('Playing!')}>
+//                 Play Movie
+//             </Button>
+//             <Button onSmash={() => alert('Uploading!')}>
+//                 Upload Image
+//             </Button>
+//         </div>
+//     );
+// }
 
 export default function App() {
     return (
+        <Toolbar
+            onPlayMovie={() => alert('Playing!')}
+            onUploadImage={() => alert('Uploading!')}
+        />
+    );
+}
+
+function Toolbar({onPlayMovie, onUploadImage}) {
+    return (
         <div>
-            <Button onSmash={() => alert('Playing!')}>
+            <Button onClick={onPlayMovie}>
                 Play Movie
             </Button>
-            <Button onSmash={() => alert('Uploading!')}>
+            <Button onClick={onUploadImage}>
                 Upload Image
             </Button>
         </div>
+    );
+}
+
+function Button({onClick, children}) {
+    return (
+        <button onClick={onClick}>
+            {children}
+        </button>
     );
 }
