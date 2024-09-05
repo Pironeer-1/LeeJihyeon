25d490dfce2afe823d9dc9bea91b36f203fee037
Jihyeon Lee <ljh130334@naver.com>
2024-09-06 01:31:38 +0900

    [Upload]React_Master_04-7

--- React_Master_04/src/App.js
+++ React_Master_04/src/App.js
@@ -130,83 +130,142 @@
 //     )
 // }
 
-import { useImmer } from 'use-immer';
+// import { useImmer } from 'use-immer';
+//
+// export default function Form() {
+//     const [person, updatePerson] = useImmer({
+//         name: 'Niki de Saint Phalle',
+//         artwork: {
+//             title: 'Blue Nana',
+//             city: 'Hamburg',
+//             image: 'https://i.imgur.com/Sd1AgUOm.jpg',
+//         }
+//     });
+//
+//     function handleNameChange(e) {
+//         updatePerson(draft => {
+//             draft.name = e.target.value;
+//         });
+//     }
+//
+//     function handleTitleChange(e) {
+//         updatePerson(draft => {
+//             draft.artwork.title = e.target.value;
+//         });
+//     }
+//
+//     function handleCityChange(e) {
+//         updatePerson(draft => {
+//             draft.artwork.city = e.target.value;
+//         });
+//     }
+//
+//     function handleImageChange(e) {
+//         updatePerson(draft => {
+//             draft.artwork.image = e.target.value;
+//         });
+//     }
+//
+//     return (
+//         <>
+//             <label>
+//                 Name:
+//                 <input
+//                     value={person.name}
+//                     onChange={handleNameChange}
+//                 />
+//             </label>
+//             <label>
+//                 Title:
+//                 <input
+//                     value={person.artwork.title}
+//                     onChange={handleTitleChange}
+//                 />
+//             </label>
+//             <label>
+//                 City:
+//                 <input
+//                     value={person.artwork.city}
+//                     onChange={handleCityChange}
+//                 />
+//             </label>
+//             <label>
+//                 Image:
+//                 <input
+//                     value={person.artwork.image}
+//                     onChange={handleImageChange}
+//                 />
+//             </label>
+//             <p>
+//                 <i>{person.artwork.title}</i>
+//                 {' by '}
+//                 {person.name}
+//                 <br/>
+//                 (located in {person.artwork.city})
+//             </p>
+//             <img
+//                 src={person.artwork.image}
+//                 alt={person.artwork.title}
+//             />
+//         </>
+//     );
+// }
 
-export default function Form() {
-    const [person, updatePerson] = useImmer({
-        name: 'Niki de Saint Phalle',
-        artwork: {
-            title: 'Blue Nana',
-            city: 'Hamburg',
-            image: 'https://i.imgur.com/Sd1AgUOm.jpg',
-        }
-    });
+import { useState } from 'react';
 
-    function handleNameChange(e) {
-        updatePerson(draft => {
-            draft.name = e.target.value;
-        });
-    }
+let nextId = 3;
+const initialList = [
+    { id: 0, title: 'Big Bellies', seen: false },
+    { id: 1, title: 'Lunar Landscape', seen: false },
+    { id: 2, title: 'Terracotta Army', seen: true },
+];
 
-    function handleTitleChange(e) {
-        updatePerson(draft => {
-            draft.artwork.title = e.target.value;
-        });
-    }
+export default function BucketList() {
+    const [list, setList] = useState(
+        initialList
+    );
 
-    function handleCityChange(e) {
-        updatePerson(draft => {
-            draft.artwork.city = e.target.value;
-        });
-    }
-
-    function handleImageChange(e) {
-        updatePerson(draft => {
-            draft.artwork.image = e.target.value;
-        });
+    function handleToggle(artworkId, nextSeen) {
+        setList(list.map(artwork => {
+            if (artwork.id === artworkId) {
+                return { ...artwork, seen: nextSeen };
+            } else {
+                return artwork;
+            }
+        }));
     }
 
     return (
         <>
-            <label>
-                Name:
-                <input
-                    value={person.name}
-                    onChange={handleNameChange}
-                />
-            </label>
-            <label>
-                Title:
-                <input
-                    value={person.artwork.title}
-                    onChange={handleTitleChange}
-                />
-            </label>
-            <label>
-                City:
-                <input
-                    value={person.artwork.city}
-                    onChange={handleCityChange}
-                />
-            </label>
-            <label>
-                Image:
-                <input
-                    value={person.artwork.image}
-                    onChange={handleImageChange}
-                />
-            </label>
-            <p>
-                <i>{person.artwork.title}</i>
-                {' by '}
-                {person.name}
-                <br/>
-                (located in {person.artwork.city})
-            </p>
-            <img
-                src={person.artwork.image}
-                alt={person.artwork.title}
-            />
+            <h1>Art Bucket List</h1>
+            <h2>My list of art to see:</h2>
+            <ItemList
+                artworks={list}
+                onToggle={handleToggle} />
         </>
+    );
+}
+
+function ItemList({ artworks, onToggle }) {
+    return (
+        <ul>
+            {artworks.map(artwork => (
+                <li key={artwork.id}>
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={artwork.seen}
+                            onChange={e => {
+                                onToggle(
+                                    artwork.id,
+                                    e.target.checked
+                                );
+                            }}
+                        />
+                        {artwork.title}
+                    </label>
+                </li>
+            ))}
+        </ul>
     );
 }
