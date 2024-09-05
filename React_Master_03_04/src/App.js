befea283eed27c253231e88c206a229e8faaeb1a
Jihyeon Lee <ljh130334@naver.com>
2024-09-05 23:23:39 +0900

    [Update]React_Master_03_04_06

--- React_Master_03_04/src/App.js
+++ React_Master_03_04/src/App.js
@@ -41,15 +41,41 @@
 //     );
 // }
 
+// export default function TodoList() {
+//     return (
+//         <ul style={{
+//             backgroundColor: 'black',
+//             color: 'lightgreen'
+//         }}>
+//             <li>Improve the videophone</li>
+//             <li>Prepare aeronautics lectures</li>
+//             <li>Work on the alcohol-fuelled engine</li>
+//         </ul>
+//     );
+// }
+
+const person = {
+    name: 'Gregorio Y. Zara',
+    theme: {
+        backgroundColor: 'black',
+        color: 'pink'
+    }
+};
+
 export default function TodoList() {
     return (
-        <ul style={{
-            backgroundColor: 'black',
-            color: 'lightgreen'
-        }}>
-            <li>Improve the videophone</li>
-            <li>Prepare aeronautics lectures</li>
-            <li>Work on the alcohol-fuelled engine</li>
-        </ul>
+        <div style={person.theme}>
+            <h1>{person.name}'s Todos</h1>
+            <img
+                className="avatar"
+                src="https://i.imgur.com/7vQD0fPs.jpg"
+                alt="Gregorio Y. Zara"
+            />
+            <ul>
+                <li>Improve the videophone</li>
+                <li>Prepare aeronautics lectures</li>
+                <li>Work on the alcohol-fuelled engine</li>
+            </ul>
+        </div>
     );
 }
