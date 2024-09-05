3027b4cd9516df1cfa63c835b2dbb40f1803dbd7
Jihyeon Lee <ljh130334@naver.com>
2024-09-06 02:13:05 +0900

    [Update]React_Master_04_03_02

--- React_Master_04_03/src/index.js
+++ React_Master_04_03/src/index.js
@@ -1,5 +1,18 @@
-import Gallery from './Gallery.js';
-import { createRoot } from 'react-dom/client';
+// import Gallery from './Gallery.js';
+// import { createRoot } from 'react-dom/client';
+//
+// const root = createRoot(document.getElementById('root'))
+// root.render(<Gallery />);
 
-const root = createRoot(document.getElementById('root'))
-root.render(<Gallery />);
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
