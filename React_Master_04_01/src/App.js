72f3595f2e4500fdc92b0eb8b1058d48cf794639
Jihyeon Lee <ljh130334@naver.com>
2024-09-06 01:44:40 +0900

    [Update]React_Master_04_01_04

--- React_Master_04_01/src/App.js
+++ React_Master_04_01/src/App.js
@@ -10,23 +10,60 @@
 //     );
 // }
 
-function AlertButton({message, children}) {
+// function AlertButton({message, children}) {
+//     return (
+//         <button onClick={() => alert(message)}>
+//             {children}
+//         </button>
+//     );
+// }
+//
+// export default function Toolbar() {
+//     return (
+//         <div>
+//             <AlertButton message="Playing!">
+//                 Play Movie
+//             </AlertButton>
+//             <AlertButton message="Uploading!">
+//                 Upload Image
+//             </AlertButton>
+//         </div>
+//     );
+// }
+
+function Button({ onClick, children }) {
     return (
-        <button onClick={() => alert(message)}>
+        <button onClick={onClick}>
             {children}
         </button>
+    );
+}
+
+function PlayButton({ movieName }) {
+    function handlePlayClick() {
+        alert(`Playing ${movieName}!`);
+    }
+
+    return (
+        <Button onClick={handlePlayClick}>
+            Play "{movieName}"
+        </Button>
+    );
+}
+
+function UploadButton() {
+    return (
+        <Button onClick={() => alert('Uploading!')}>
+            Upload Image
+        </Button>
     );
 }
 
 export default function Toolbar() {
     return (
         <div>
-            <AlertButton message="Playing!">
-                Play Movie
-            </AlertButton>
-            <AlertButton message="Uploading!">
-                Upload Image
-            </AlertButton>
+            <PlayButton movieName="Kiki's Delivery Service" />
+            <UploadButton />
         </div>
     );
 }
