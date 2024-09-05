21d18ea79c99918b96121870581b7da69bd46497
Jihyeon Lee <ljh130334@naver.com>
2024-09-05 23:16:15 +0900

    [Update]React_Master_03_04_02

--- React_Master_03_04/src/App.js
+++ React_Master_03_04/src/App.js
@@ -1,9 +1,21 @@
+// export default function Avatar() {
+//   return (
+//       <img
+//         className="avatar"
+//         src="https://i.imgur.com/7vQD0fPs.jpg"
+//         alt="Gregorio Y. Zara"
+//       />
+//   );
+// }
+
 export default function Avatar() {
-  return (
-      <img
-        className="avatar"
-        src="https://i.imgur.com/7vQD0fPs.jpg"
-        alt="Gregorio Y. Zara"
-      />
-  );
+    const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+    const description = 'Gregorio Y. Zara';
+    return (
+        <img
+            className="avatar"
+            src={avatar}
+            alt={description}
+        />
+    );
 }
