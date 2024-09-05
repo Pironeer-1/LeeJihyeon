a8a53aa312302669482ac938195445a3bf058c8a
Jihyeon Lee <ljh130334@naver.com>
2024-09-06 01:51:05 +0900

    [Update]React_Master_04_01_09

--- React_Master_04_01/src/App.js
+++ React_Master_04_01/src/App.js
@@ -134,28 +134,37 @@
 //     );
 // }
 
-function Button({ onClick, children }) {
-    return (
-        <button onClick={e => {
-            e.stopPropagation();
-            onClick();
-        }}>
-            {children}
-        </button>
-    );
-}
+// function Button({ onClick, children }) {
+//     return (
+//         <button onClick={e => {
+//             e.stopPropagation();
+//             onClick();
+//         }}>
+//             {children}
+//         </button>
+//     );
+// }
+//
+// export default function Toolbar() {
+//     return (
+//         <div className="Toolbar" onClick={() => {
+//             alert('You clicked on the toolbar!');
+//         }}>
+//             <Button onClick={() => alert('Playing!')}>
+//                 Play Movie
+//             </Button>
+//             <Button onClick={() => alert('Uploading!')}>
+//                 Upload Image
+//             </Button>
+//         </div>
+//     );
+// }
 
-export default function Toolbar() {
+export default function Signup() {
     return (
-        <div className="Toolbar" onClick={() => {
-            alert('You clicked on the toolbar!');
-        }}>
-            <Button onClick={() => alert('Playing!')}>
-                Play Movie
-            </Button>
-            <Button onClick={() => alert('Uploading!')}>
-                Upload Image
-            </Button>
-        </div>
+        <form onSubmit={() => alert('Submitting!')}>
+            <input />
+            <button>Send</button>
+        </form>
     );
 }
