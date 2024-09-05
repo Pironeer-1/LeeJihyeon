71a26669033d91a2ce8367cbec6fb55ad8eb711d
Jihyeon Lee <ljh130334@naver.com>
2024-09-06 02:29:24 +0900

    [Update]React_Master_04_05_03

--- React_Master_04_05/src/App.js
+++ React_Master_04_05/src/App.js
@@ -5,15 +5,20 @@ export default function Counter() {
 
   return (
       <>
-        <h1>{number}</h1>
-        <button onClick={() => {
-          // setNumber(number + 1);
-          // setNumber(number + 1);
-          // setNumber(number + 1);
-            setNumber(n => n + 1);
-            setNumber(n => n + 1);
-            setNumber(n => n + 1);
-        }}>+3</button>
+          <h1>{number}</h1>
+          {/*<button onClick={() => {*/}
+          {/*  // setNumber(number + 1);*/}
+          {/*  // setNumber(number + 1);*/}
+          {/*  // setNumber(number + 1);*/}
+          {/*    setNumber(n => n + 1);*/}
+          {/*    setNumber(n => n + 1);*/}
+          {/*    setNumber(n => n + 1);*/}
+          {/*}}>+3</button>*/}
+          <button onClick={() => {
+              setNumber(number + 5);
+              setNumber(n => n + 1);
+          }}>Increase the number
+          </button>
       </>
   )
 }
